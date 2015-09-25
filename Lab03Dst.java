6f599526c2133dde259aef2d39eba8cd66a9838a
unknown <4070324@HS222-12.vtsd.com>
2015-09-25 11:58:15 -0400

    Zazzara-Lab-03

---
+++ Lab03Dst.java
@@ -0,0 +1,48 @@
+// Lab03Dst.java
+// The Visible Average Program
+// This is the student, starting version, of Lab 03C.
+
+
+public class Lab03Dst
+{
+    public static void main(String args[])
+    {
+        System.out.println();
+        System.out.println("Lab 03D");
+        System.out.println();
+        int num1 = 11;
+        int num2 = 22;
+        int num3 = 33;
+        int num4 = 44;
+        int num5 = 55;
+        System.out.println ("------------------");
+        System.out.println ("1st integer: " + num1);
+        System.out.println ("2nd integer: " + num2);
+        System.out.println ("3rd integer: " + num3);
+        System.out.println ("4th integer: " + num4);
+        System.out.println ("5th integer: " + num5);
+        System.out.println ("------------------");
+        int Total = 165;
+        int Average = 33;
+        System.out.println ("Integer Total: " + Total);
+        System.out.println ("Integer Total: " + Average);
+        System.out.println ("------------------");
+        double num6 = 55.55;
+        double num7 = 66.66;
+        double num8 = 77.77;
+        double num9 = 88.88;
+        double num0 = 99.99;
+        System.out.println ("1st real number: " + num6);
+        System.out.println ("2nd real number: " + num7);
+        System.out.println ("3rd real number: " + num8);
+        System.out.println ("4th real number: " + num9);
+        System.out.println ("5th real number: " + num0);
+        System.out.println ("------------------");
+        double DTotal = 388.85;
+        double DAverage = 77.77000000000001;
+        System.out.println("Double Total: " + DTotal);
+        System.out.println("Double Average: " + DAverage);
+        System.out.println ("------------------");
+        System.out.println ();
+    }
+}
